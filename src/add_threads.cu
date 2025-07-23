b2ce257dbbacf0ec0b5637b27257a72c31165453
Ilya Pakhalko <ilyaphlk@github.com>
2025-07-24 00:21:36 +0300

    simple test program

--- src/add_threads.cu
+++ src/add_threads.cu
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
-__global__ void add_threads(int* s) {
-    *s = threadIdx.x + blockIdx.x;
+__global__ void add_threads(int* s, const int n) {
+    *s = threadIdx.x + n * blockIdx.x;
 }
 
 int main() {
     int sum = 0;
-    add_threads<<<2, 2>>>(&sum);
+    const int n = 8;
+    add_threads<<<n, n>>>(&sum, n);
 
     cudaError_t err = cudaDeviceSynchronize();
     if (err != cudaSuccess) {
