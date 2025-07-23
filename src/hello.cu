ad29d275044ab103b7b14191460e8b5fba22d9ad
Ilya Pakhalko <ilyaphlk@github.com>
2025-07-23 03:34:33 +0300

    write buffer to hardcoded file as well

--- src/hello.cu
+++ src/hello.cu
@@ -29,6 +29,10 @@ int main() {
     cudaMemcpy(h_buffer, d_buffer, 256, cudaMemcpyDeviceToHost);
     printf("%s", h_buffer);
 
+    FILE *f = fopen("output.txt", "w");
+    fprintf(f, "%s\n", h_buffer);
+    fclose(f);
+
     cudaFree(d_buffer);
 
     return 0;
