25c65705d74aa2659d005898fd17eb9fad66106e
Ilya Pakhalko <ilyaphlk@github.com>
2025-07-23 03:10:29 +0300

    write to device-side buffer

--- src/hello.cu
+++ src/hello.cu
@@ -1,17 +1,26 @@
 #include <cstdio>
 #include <cuda_runtime.h>
 
-__global__ void hello() {
-    printf("hello, world! thread number %d\n", threadIdx.x);
+__global__ void hello(char* buffer) {
+    sprintf(buffer, "hello, world! thread number %d\n", threadIdx.x);
 }
 
 int main() {
-    hello<<<1, 1>>>();
+    char* d_buffer;
+    cudaMalloc(&d_buffer, 256);
+
+    hello<<<1, 1>>>(d_buffer);
 
     cudaError_t err = cudaDeviceSynchronize();
     if (err != cudaSuccess) {
         printf("CUDA error: %s\n", cudaGetErrorString(err));
     }
+
+    char h_buffer[256];
+    cudaMemcpy(h_buffer, d_buffer, 256, cudaMemcpyDeviceToHost);
+    printf("%s", h_buffer);
+
+    cudaFree(d_buffer);
 
     return 0;
 }
