71d5a60c5468fb954ea00a0053d875dfce501755
Ilya Pakhalko <ilyaphlk@github.com>
2025-08-03 19:02:15 +0300

    add_threads.cu

--- src/add_threads.cu
+++ src/add_threads.cu
@@ -2,19 +2,23 @@
 #include <cuda_runtime.h>
 
 __global__ void add_threads(int* s, const int n) {
-    //*s = threadIdx.x + n * blockIdx.x;
-    *s = n;
+    *s = threadIdx.x + n * blockIdx.x;
 }
 
 int main() {
-    int sum = 0;
+    int hSum = 0;
+    int* dSum;
+    CudaMalloc(&dSum, sizeof(int));
     const int n = 8;
-    add_threads<<<n, n>>>(&sum, n);
+    add_threads<<<1, n>>>(&dSum, n);
 
     cudaError_t err = cudaDeviceSynchronize();
     if (err != cudaSuccess) {
         printf("CUDA error: %s\n", cudaGetErrorString(err));
     }
 
-    printf("sum is %d\n", sum);
+    CudaMemcpy(&hSum, dSum);
+    CudaFree(dSum);
+
+    printf("sum is %d\n", hSum);
 }
